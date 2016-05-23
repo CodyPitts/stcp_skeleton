fc82c6bac65a3e41f9da35202423f979ab97f907
jacobrnew <jacobrnew@gmail.com>
2016-05-22 18:42:15 -0700

    Finished 3 Way Handshake
    -still need to fix connection failures with connection close

--- transport.c
+++ transport.c
@@ -27,15 +27,16 @@ enum { CSTATE_ESTABLISHED, CSTATE_HANDSHAKING, CSTATE_CLOSING, CSTATE_CLOSED }; 
 /* this structure is global to a mysocket descriptor */
 typedef struct
 {
-    bool_t done;    /* TRUE once connection is closed */
-
-    int connection_state;   /* state of the connection (established, etc.) */
-    tcp_seq initial_sequence_num;
-	tcp_seq curr_sequence_num;
-
-    /* any other connection-wide global variables go here */
-	tcp_seq byteWindow = 3072;
-	tcphdr* buffer;
+  bool_t done;    /* TRUE once connection is closed */
+  
+  int connection_state;   /* state of the connection (established, etc.) */
+  tcp_seq initial_sequence_num;
+  tcp_seq curr_sequence_num;
+  
+  /* any other connection-wide global variables go here */
+  tcp_seq byteWindow = 3072;
+  tcphdr* buffer;
+  
 } context_t;
 
 
@@ -49,128 +50,126 @@ static void control_loop(mysocket_t sd, context_t *ctx);
  */
 void transport_init(mysocket_t sd, bool_t is_active)
 {
-    context_t *ctx;
+  context_t *ctx;
+  ctx = (context_t *) calloc(1, sizeof(context_t));
+  assert(ctx);
+  ctx->buffer = (tcphdr*)calloc(1,sizeof(tcphdr));
+  assert(ctx->buffer);
 
-    ctx = (context_t *) calloc(1, sizeof(context_t));
-    assert(ctx);
-
-    generate_initial_seq_num(ctx);
-	ctx->curr_sequence_num = ctx->initial_sequence_num;
-
-    /* XXX: you should send a SYN packet here if is_active, or wait for one
-     * to arrive if !is_active.  after the handshake completes, unblock the
-     * application with stcp_unblock_application(sd).  you may also use
-     * this to communicate an error condition back to the application, e.g.
-     * if connection fails; to do so, just set errno appropriately (e.g. to
-     * ECONNREFUSED, etc.) before calling the function.
-     */
-	ctx -> connection_state = CSTATE_HANDSHAKING;
-	//KEEP TRACK OF ACK AND SEQ #s
-	if (is_active) {
-		// creating SYN header
-		tcphdr *synhdr;
-		synhdr = (tcphdr *)calloc(1, sizeof(tcphdr));
-		assert(synhdr);
-		// Setting flags in header
-		synhdr->th_seq = ctx->curr_sequence_num;
-		//synhdr->th_ack = synhdr->th_seq++;
-		synhdr->th_flags = TH_SYN;
-		// synhdr->th_win = synhdr->th_ack + ctx->byteWindow - 1;
-		// First handshake
-		// maybe synhdr->th_win instead of sizeof(...)
-		if ((stcp_network_send(sd, synhdr, sizeof(tcphdr))) == -1){
-			//close_connection();
-		}
-		printf("HERE");
-		// Recieving from network requires setting the correct recv window
-		if ((stcp_network_recv(sd, (void*)ctx->buffer, sizeof(tcphdr))) == -1){
-			//close_connection();
-		}
-		// See if packet recv is the SYN_ACK packet
-		// Bitwise and to check for both the SYN flag and ACK flag
-		if (ctx->buffer->th_flags & (TH_SYN | TH_ACK)){
-			// Check to see if peer's ack seq# is + 1 our SYN's seq#
-			if (ctx->buffer->th_ack == synhdr->th_seq + 1){
-				ctx->curr_sequence_num++;
-				// creating ACK header for last handshake
-				tcphdr *ackhdr;
-				ackhdr = (tcphdr *)calloc(1, sizeof(ackhdr));
-				assert(ackhdr);
-				//ackhdr->th_seq = ctx->buffer->th_seq+1;
-				ackhdr->th_ack = ctx->buffer->th_seq;
-				ackhdr->th_flags = TH_SYN;
-				//ackhdr->th_win = ackhdr->th_seq + ctx->byteWindow - 1;
-				if ((stcp_network_send(sd, ackhdr, sizeof(tcphdr))) == -1){
-					//close_connection();
-				}
-			}
-		}
-		//simultaneous syns sent
-		else if(ctx->buffer->th_flags & TH_SYN) {
-			//send SYN ACK, with our previous SEQ number, and their SEQ + 1
-			//Wait on SYN ACK with our SEQ number +1 and their SEQ number again
-			tcphdr *synack;
-			synack = (tcphdr*)calloc(1, sizeof(tcphdr));
-			assert(synack);
-			synack->th_seq = ctx->curr_sequence_num;
-			synack->th_ack = ctx->buffer->th_seq++;
-			if ((stcp_network_send(sd, synack, sizeof(tcphdr))) == -1){
-				//close_connection();
-			}
-		}
-		//wrong flags
-		else {
-			//close_connection();
-		}
-	} else {
-		if ((stcp_network_recv(sd, (void*)ctx->buffer, sizeof(tcphdr))) == -1){
-			//close_connection();
-		}
-		if (ctx->buffer->th_flags & TH_SYN) {
-			//send SYN ACK, with our previous SEQ number, and their SEQ + 1
-			//Wait on SYN ACK with our SEQ number +1 and their SEQ number again
-			tcphdr *synack;
-			synack = (tcphdr*)calloc(1, sizeof(synack));
-			assert(synack);
-			synack->th_seq = ctx->curr_sequence_num;
-			synack->th_ack = ctx->buffer->th_seq++;
-			if ((stcp_network_send(sd, synack, sizeof(tcphdr))) == -1){
-				//close_connection();
-			}
-			if ((stcp_network_recv(sd, (void*)ctx->buffer, sizeof(tcphdr))) == -1){
-				//close_connection();
-			}
-			if (!(ctx->buffer->th_flags & TH_ACK)
-				|| !(ctx->buffer->th_seq == ctx->curr_sequence_num+1)){
-				// close_connection();
-			}
-		}
-		//is_active = true;
+  generate_initial_seq_num(ctx);
+  ctx->curr_sequence_num = ctx->initial_sequence_num;
+  
+  /* XXX: you should send a SYN packet here if is_active, or wait for one
+   * to arrive if !is_active.  after the handshake completes, unblock the
+   * application with stcp_unblock_application(sd).  you may also use
+   * this to communicate an error condition back to the application, e.g.
+   * if connection fails; to do so, just set errno appropriately (e.g. to
+   * ECONNREFUSED, etc.) before calling the function.
+   */
+  ctx -> connection_state = CSTATE_HANDSHAKING;
+  //KEEP TRACK OF ACK AND SEQ #s
+  if (is_active) {
+	// creating SYN header
+	tcphdr *synhdr;
+	synhdr = (tcphdr *)calloc(1, sizeof(tcphdr));
+	assert(synhdr);
+	// Setting flags in header
+	synhdr->th_seq = ctx->curr_sequence_num;
+	synhdr->th_flags = TH_SYN;
+	// First handshake
+	// maybe synhdr->th_win instead of sizeof(...)
+	if ((stcp_network_send(sd, synhdr, sizeof(tcphdr),NULL)) == -1){
+	  //close_connection();
 	}
-
-    ctx->connection_state = CSTATE_ESTABLISHED;
-    stcp_unblock_application(sd);
-
-    control_loop(sd, ctx);
-
-    /* do any cleanup here */
-    free(ctx);
+	// Recieving from network requires setting the correct recv window
+	if ((stcp_network_recv(sd, (void*)ctx->buffer, sizeof(tcphdr)))
+		== -1){
+	  //close_connection();
+	}
+	// See if packet recv is the SYN_ACK packet
+	// Bitwise and to check for both the SYN flag and ACK flag
+	if (ctx->buffer->th_flags & (TH_SYN | TH_ACK)){
+	  // Check to see if peer's ack seq# is + 1 our SYN's seq#
+	  if (ctx->buffer->th_ack == synhdr->th_seq + 1){
+		ctx->curr_sequence_num++;
+		// creating ACK header for last handshake
+		tcphdr *ackhdr;
+		ackhdr = (tcphdr *)calloc(1, sizeof(ackhdr));
+		assert(ackhdr);
+		ackhdr->th_ack = ctx->buffer->th_seq+1;
+		ackhdr->th_flags = TH_ACK;
+		if ((stcp_network_send(sd, ackhdr, sizeof(tcphdr),NULL)) == -1){
+		  //close_connection();
+		}
+	  }
+	}
+	//simultaneous syns sent
+	else if(ctx->buffer->th_flags & TH_SYN) {
+	  //send SYN ACK, with our previous SEQ number, and their SEQ + 1
+	  //Wait on SYN ACK with our SEQ number +1 and their SEQ number again
+	  tcphdr *synack;
+	  synack = (tcphdr*)calloc(1, sizeof(tcphdr));
+	  assert(synack);
+	  synack->th_seq = ctx->curr_sequence_num;
+	  synack->th_ack = ctx->buffer->th_seq++;
+	  if ((stcp_network_send(sd, synack, sizeof(tcphdr),NULL)) == -1){
+		//close_connection();
+	  }
+	}
+	//wrong flags
+	else {
+	  //close_connection();
+	}
+  } else {
+	if ((stcp_network_recv(sd, (void*)ctx->buffer, sizeof(tcphdr)))
+		== -1){
+	  //close_connection();
+	}
+	if (ctx->buffer->th_flags & TH_SYN) {
+	  //send SYN ACK, with our previous SEQ number, and their SEQ + 1
+	  //Wait on SYN ACK with our SEQ number +1 and their SEQ number again
+	  tcphdr *synack;
+	  synack = (tcphdr*)calloc(1, sizeof(synack));
+	  assert(synack);
+	  synack->th_seq = ctx->curr_sequence_num;
+	  synack->th_ack = ctx->buffer->th_seq++;
+	  if ((stcp_network_send(sd, synack, sizeof(tcphdr),NULL)) == -1){
+		//close_connection();
+	  }
+	  if ((stcp_network_recv(sd, (void*)ctx->buffer, sizeof(tcphdr)))
+		  == -1){
+		//close_connection();
+	  }
+	  if (!(ctx->buffer->th_flags & TH_ACK)
+		  || !(ctx->buffer->th_seq == ctx->curr_sequence_num+1)){
+		// close_connection();
+	  }
+	}
+  }
+  
+  ctx->connection_state = CSTATE_ESTABLISHED;
+  stcp_unblock_application(sd);
+  
+  control_loop(sd, ctx);
+  
+  /* do any cleanup here */
+  free(ctx);
 }
 
 
 /* generate random initial sequence number for an STCP connection */
 static void generate_initial_seq_num(context_t *ctx)
 {
-    assert(ctx);
-
+  assert(ctx);
+  
 #ifdef FIXED_INITNUM
-    /* please don't change this! */
-    ctx->initial_sequence_num = 1;
+  /* please don't change this! */
+  ctx->initial_sequence_num = 1;
 #else
-/* you have to fill this up */
-    /*ctx->initial_sequence_num = RAND_NUM;*/
-	srand(time(NULL));
-	ctx->initial_sequence_num = rand() % 255;
+  /* you have to fill this up */
+  /*ctx->initial_sequence_num = RAND_NUM;*/
+  srand(time(NULL));
+  ctx->initial_sequence_num = rand() % 255;
 #endif
 }
 
@@ -184,26 +183,24 @@ static void generate_initial_seq_num(context_t *ctx)
  */
 static void control_loop(mysocket_t sd, context_t *ctx)
 {
-    assert(ctx);
-    assert(!ctx->done);
-
-    while (!ctx->done)
-    {
-        unsigned int event;
-
-        /* see stcp_api.h or stcp_api.c for details of this function */
-        /* XXX: you will need to change some of these arguments! */
-        event = stcp_wait_for_event(sd, 0, NULL);
-
-        /* check whether it was the network, app, or a close request */
-        if (event & APP_DATA)
-        {
-            /* the application has requested that data be sent */
-            /* see stcp_app_recv() */
-        }
-
-        /* etc. */
-    }
+  assert(ctx);
+  assert(!ctx->done);
+  
+  while (!ctx->done){
+	unsigned int event;
+	
+	/* see stcp_api.h or stcp_api.c for details of this function */
+	/* XXX: you will need to change some of these arguments! */
+	event = stcp_wait_for_event(sd, 0, NULL);
+	
+	/* check whether it was the network, app, or a close request */
+	if (event & APP_DATA){
+	  /* the application has requested that data be sent */
+	  /* see stcp_app_recv() */
+	}
+	
+	/* etc. */
+  }
 }
 
 
